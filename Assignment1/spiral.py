ce774dd49361ea0572fcbfc992f4086788a1c9b1
Cole Schuelke <coleschuelke@gmail.com>
2023-08-29 22:30:54 -0500

    prints matrix and adds surrounding values

--- Assignment1/spiral.py
+++ Assignment1/spiral.py
@@ -10,9 +10,9 @@
   Partner UT EID:
 
   Course Name: CS 313E
-  Unique Number: 
-  Date Created:
-  Date Last Modified:
+  Unique Number: 52590
+  Date Created: 08/26/23
+  Date Last Modified: 08/29/23
 
  Input: n is an odd integer between 1 and 100
  Output: returns a 2-D list representing a spiral
@@ -44,25 +44,26 @@ def print_spiral(spiral):
 
 def create_spiral(dim):
     """Creates a Spiral given a dimension for the spiral dimeter"""
+    # function variables
     side_length = 2
     mid = math.floor(dim/2)
     ring = 1
-    # forward = True
     num_to_add = 3
 
     
-        # initialize empty matrix of the given size
+    # initialize empty matrix of the given size
     spiral = [0] * dim
     for i in range(dim):
         spiral[i] = [0] * dim
-    print_spiral(spiral)
+    # print_spiral(spiral)
 
+    # handle the first few numbers
     if dim == 1:
         spiral[0][0] = 1
     else:
         spiral[mid][mid] = 1
         spiral[mid][mid+1] = 2
-        print_spiral(spiral)
+        # print_spiral(spiral)
         while True :
             # bottom
             for col in range(mid+ring, mid+ring-side_length, -1):
@@ -70,7 +71,7 @@ def create_spiral(dim):
                     break
                 spiral[mid+ring][col] = num_to_add
                 num_to_add +=1
-                print_spiral(spiral)
+                # print_spiral(spiral)
 
             # left
             for row in range(mid+ring+1, mid+ring-side_length+1, -1):
@@ -100,19 +101,11 @@ def create_spiral(dim):
 
             side_length +=1
             ring +=1
-            print_spiral(spiral)
-            print(f"made it to ring {ring}")
-        print_spiral(spiral)
-        print(spiral)
-
-
-    
-
-
-
-        
-  
-
+            # print_spiral(spiral)
+            # print(f"made it to ring {ring}")
+        # print_spiral(spiral)
+        # print(spiral)
+    return spiral
 
 
 def sum_sub_grid(grid, val):
@@ -125,14 +118,43 @@ def sum_sub_grid(grid, val):
     surrounding the parameter val in the grid
     if val is out of bounds, returns 0
     """
+    dim = len(grid)
+    # values to handle edge cases
+    upper = 2
+    lower = -1
+    left = -1
+    right = 2
+    # if val > dim ** 2:
+    #     return 0
 
-    # I think it should just iterate over the rows or columns, then check all the sides
-    # set some row and column parameters and an if statement that checks if it's valid 
-    
-    # ADD YOUR CODE HERE  
+    # find the index of val
+    index = (0, 0)
+    for i in range(dim):
+        for j in range(dim):
+            if grid[i][j] == val:
+                index = (i,j)
 
+    print(index)
 
-    return # ADD YOUR CODE HERE  
+    # check for edge cases
+    if index[0] == 0:
+        lower = 0
+    if index[0] == dim-1:
+        upper = 1
+    if index[1] == 0:
+        left = 0
+    if index[1] == dim-1:
+        right = 1
+
+    sum = 0
+
+    # sum up the block of numbers
+    for i in range(lower, upper): # rows
+        for j in range(left, right): # cols
+            sum += grid[index[0]+i][index[1]+j]
+    # don't include the number itself in the tally
+    sum -= val
+    return sum  
 
 
 
